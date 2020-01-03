8357c222d67830ee9cefb3cced51103928745090
iwasaka <ne300057@senshu-u.jp>
2020-01-04 00:22:54 +0900

    Fakerの導入とItemテーブルにテストデータ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: "ゲームソフト")
+
+10.times do |n|
+  category_id = 1
+  name  = Faker::Name.name
+  body = Faker::Lorem.paragraphs
+  stock = 100
+  price = 5000
+  release = Date.today
+  Item.create(category_id: category_id,name: name,body: body,stock: stock,price: price,release: release)
+end
